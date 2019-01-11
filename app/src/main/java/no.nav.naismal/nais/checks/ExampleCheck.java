1d2230e56c9b69bd387ec0e08d31a221e9da6242
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2019-01-11 12:22:08 +0100

    MMA-2138 Endret logback-spring for å unngå krasj, og oppdatert ExampleCheck

--- app/src/main/java/no.nav.naismal/nais/checks/ExampleCheck.java
+++ app/src/main/java/no.nav.naismal/nais/checks/ExampleCheck.java
@@ -1,29 +1,27 @@
-//package no.nav.naismal.nais.checks;
-//
-//import no.nav.naismal.nais.selftest.AbstractDependencyCheck;
-//import no.nav.naismal.nais.selftest.ApplicationNotReadyException;
-//import no.nav.naismal.nais.selftest.DependencyType;
-//import no.nav.naismal.nais.selftest.Importance;
-//import org.springframework.stereotype.Component;
-//@Component
-//public class ExampleCheck extends AbstractDependencyCheck {
-//
-//    private final Consumer consumer;
-//
-//    public ExampleCheck(Consumer consumer) {
-//        super(DependencyType type, String name, String address, Importance importance);
-//        this.consumer = consumer;
-//    }
-//
-//
-//    @Override
-//    protected void doCheck() {
-//        try {
+package no.nav.naismal.nais.checks;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import no.nav.naismal.nais.selftest.AbstractDependencyCheck;
+import no.nav.naismal.nais.selftest.ApplicationNotReadyException;
+import no.nav.naismal.nais.selftest.DependencyType;
+import no.nav.naismal.nais.selftest.Importance;
+import org.springframework.stereotype.Component;
+@Component
+public class ExampleCheck extends AbstractDependencyCheck {
+
+
+    public ExampleCheck(MeterRegistry registry) {
+        super(DependencyType.REST, "exampleConsumer", "exampleEndpoint", Importance.WARNING, registry);
+    }
+
+    @Override
+    protected void doCheck() {
+        try {
 //            consumer.ping();
-//        } catch (Exception e) {
-//            throw new ApplicationNotReadyException(String.format("Calling [name] failed. errorMessage=%s", getErrorMessage(e)), e);
-//        }
-//    }
-//
-//
-//}
+        } catch (Exception e) {
+            throw new ApplicationNotReadyException(String.format("Calling [name] failed. errorMessage=%s", getErrorMessage(e)), e);
+        }
+    }
+
+
+}
