03777891ba61eff9728ac31e247d3e0bc366c4a2
Sigurd Midttun <sigurd.midttun@nav.no>
2019-03-21 12:27:05 +0100

    MMA-2078 fiks på feilhåndteringsendring

--- app/src/main/java/no/nav/dokdistfordeling/qdist008/DistribuerForsendelseMapper.java
+++ app/src/main/java/no/nav/dokdistfordeling/qdist008/DistribuerForsendelseMapper.java
@@ -37,8 +37,8 @@ public class DistribuerForsendelseMapper {
 					.distribusjonbestilling(mapDokumentbestillingsinformasjon(distribuerForsendelse.getDistribusjonbestilling()))
 					.build();
 		} catch (IllegalArgumentException e) {
-			throw new DistrubuerForsendelseMapFunctionalException(format("Kunne ikke mappe qdist008-XML til domene-objekter. Feilmelding=" ,
-					distribuerForsendelse.getDistribusjonbestilling().getBestillingsId(), e.getMessage()), e);
+			throw new DistrubuerForsendelseMapFunctionalException(format("Kunne ikke mappe qdist008 input-XML til domene-objekter. Feilmelding=%s",
+					e.getMessage()), e);
 		}
 	}
 
