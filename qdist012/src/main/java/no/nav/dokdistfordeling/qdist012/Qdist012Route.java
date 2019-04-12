899f34ffecde778a00f975bfecb3a5ecbe17310b
Sigurd Midttun <sigurd.midttun@nav.no>
2019-04-12 16:53:51 +0200

    MMA-2884 Skall for qdist012

--- qdist012/src/main/java/no/nav/dokdistfordeling/qdist012/Qdist012Route.java
+++ qdist012/src/main/java/no/nav/dokdistfordeling/qdist012/Qdist012Route.java
@@ -5,8 +5,6 @@ import static org.apache.camel.LoggingLevel.ERROR;
 
 import no.nav.dokdistfordeling.exception.functional.AbstractDokdistfordelingFunctionalException;
 import no.nav.dokdistfordeling.metrics.Qdist012MetricsRoutePolicy;
-import no.nav.dokdistfordeling.qdist008.domain.DistribuerForsendelseTilSentralPrint;
-import no.nav.meldinger.virksomhet.dokdistfordeling.DistribuerForsendelse;
 import org.apache.camel.ExchangePattern;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.ValidationException;
@@ -24,6 +22,7 @@ import java.nio.charset.StandardCharsets;
 /**
  * @author Sigurd Midttun, Visma Consulting.
  */
+//FIXME
 @Component
 public class Qdist012Route extends SpringRouteBuilder {
 
@@ -76,18 +75,18 @@ public class Qdist012Route extends SpringRouteBuilder {
 				.process(exchange -> MDC.put(CALL_ID, (String) exchange.getProperty(PROPERTY_BESTILLINGS_ID)))
 				.doCatch(Exception.class)
 				.end()
-				.to("validator:no/nav/meldinger/virksomhet/dokdistfordeling/xsd/distribuerforsendelse.xsd")
-				.unmarshal(new JaxbDataFormat(JAXBContext.newInstance(DistribuerForsendelse.class)))
-				.bean(distribuerForsendelseMapper)
-				.bean(forsendelseValidator)
+//				.to("validator:no/nav/meldinger/virksomhet/dokdistfordeling/xsd/distribuerforsendelse.xsd")
+//				.unmarshal(new JaxbDataFormat(JAXBContext.newInstance(DistribuerForsendelse.class)))
+//				.bean(distribuerForsendelseMapper)
+//				.bean(forsendelseValidator)
 				.log(LoggingLevel.INFO, log, "qdist008 har validert forsendelse med bestillingsId=${exchangeProperty." + PROPERTY_BESTILLINGS_ID + "} ok.")
 				.bean(qdist012Service)
-				.marshal(new JaxbDataFormat(JAXBContext.newInstance(DistribuerForsendelseTilSentralPrint.class)))
+//				.marshal(new JaxbDataFormat(JAXBContext.newInstance(DistribuerForsendelseTilSentralPrint.class)))
 				.convertBodyTo(String.class, StandardCharsets.UTF_8.toString())
 				.setHeader(CALL_ID, simple("${exchangeProperty." + PROPERTY_BESTILLINGS_ID + "}"))
 				.inOnly("jms:" + qdist009.getQueueName())
 				.log(LoggingLevel.INFO, log, "qdist008 har lagt forsendelse med " + getIdsForLogging() + " på kø til qdist009 for distribusjon via PRINT")
-				.bean(dokdistStatusUpdater)
+//				.bean(dokdistStatusUpdater)
 				.log(LoggingLevel.INFO, log, "qdist008 har oppdatert forsendelseStatus i dokdist og avslutter behandling av forsendelse med " + getIdsForLogging());
 	}
 
