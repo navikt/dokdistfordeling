9e59049181a1ba899bc1c511daaf24cb40d168bc
Tsigab Gebremedhin <tsigab.angosom@gmail.com>
2021-02-17 09:46:37 +0100

    MMA-5165 oppgradert spring-boot og snyk sårbarhet rapport.

--- core/src/main/java/no/nav/dokdistfordeling/storage/S3Configuration.java
+++ core/src/main/java/no/nav/dokdistfordeling/storage/S3Configuration.java
@@ -9,13 +9,15 @@ import com.amazonaws.auth.policy.Statement;
 import com.amazonaws.auth.policy.actions.S3Actions;
 import com.amazonaws.auth.policy.resources.S3ObjectResource;
 import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.AwsRegionProvider;
+import com.amazonaws.regions.AwsSystemPropertyRegionProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3EncryptionClientBuilder;
+import com.amazonaws.services.s3.AmazonS3EncryptionClientV2Builder;
 import com.amazonaws.services.s3.model.BucketLifecycleConfiguration;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.CreateBucketRequest;
-import com.amazonaws.services.s3.model.CryptoConfiguration;
+import com.amazonaws.services.s3.model.CryptoConfigurationV2;
 import com.amazonaws.services.s3.model.CryptoMode;
 import com.amazonaws.services.s3.model.EncryptionMaterials;
 import com.amazonaws.services.s3.model.StaticEncryptionMaterialsProvider;
@@ -36,90 +38,96 @@ import java.util.List;
 @Profile("nais")
 public class S3Configuration {
 
-	private static final String REGION_TO_USE_FOR_S3_TO_WORK_ONPREM = Regions.US_EAST_1.name();
-	public static final String BUCKET_NAME = "dokdistmellomlager";
+    private static final String REGION_TO_USE_FOR_S3_TO_WORK_ONPREM = Regions.US_EAST_1.name();
+    public static final String BUCKET_NAME = "dokdistmellomlager";
 
-	private SecretKey secretKey;
+    private SecretKey secretKey;
 
-	@Value("${dokdistfordeling_s3_creds_username}")
-	private String credsUsername;
+    @Value("${dokdistfordeling_s3_creds_username}")
+    private String credsUsername;
 
-	@Value("${dokdistfordeling_s3_creds_password}")
-	private String credsPass;
+    @Value("${dokdistfordeling_s3_creds_password}")
+    private String credsPass;
 
-	@Value("${storage_s3_url}")
-	private String s3Endpoint;
+    @Value("${storage_s3_url}")
+    private String s3Endpoint;
 
-	@Value("${dokdistmellomlager_s3_storage_crypto_password}")
-	private String encryptionPassphrase;
+    @Value("${dokdistmellomlager_s3_storage_crypto_password}")
+    private String encryptionPassphrase;
 
-	@Bean
-	public Storage awsStorage() {
-		secretKey = key(encryptionPassphrase);
-		AmazonS3 s3 = s3(secretKey);
+    @Bean
+    public AwsRegionProvider awsRegionProvider() {
+        System.setProperty("aws.region", "us-east-1");
+        return new AwsSystemPropertyRegionProvider();
+    }
 
-		ensureBucketExists(s3);
-		configureBucketLifecycle(s3);
-		configureBucketAccessPolicy(s3);
+    @Bean
+    public Storage awsStorage() {
+        secretKey = key(encryptionPassphrase);
+        AmazonS3 s3 = s3(secretKey);
 
-		return new S3Storage(s3);
-	}
+        ensureBucketExists(s3);
+        configureBucketLifecycle(s3);
+        configureBucketAccessPolicy(s3);
 
-	private AmazonS3 s3(SecretKey secretKey) {
-		AWSCredentials credentials = new BasicAWSCredentials(credsUsername, credsPass);
+        return new S3Storage(s3);
+    }
 
-		return AmazonS3EncryptionClientBuilder
-				.standard()
-				.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(s3Endpoint, REGION_TO_USE_FOR_S3_TO_WORK_ONPREM))
-				.enablePathStyleAccess()
-				.withCredentials(new AWSStaticCredentialsProvider(credentials))
-				.withCryptoConfiguration(new CryptoConfiguration(CryptoMode.StrictAuthenticatedEncryption))
-				.withEncryptionMaterials(new StaticEncryptionMaterialsProvider(new EncryptionMaterials(secretKey)))
-				.build();
-	}
+    private AmazonS3 s3(SecretKey secretKey) {
+        awsRegionProvider();
+        AWSCredentials credentials = new BasicAWSCredentials(credsUsername, credsPass);
 
-	private void ensureBucketExists(AmazonS3 s3) {
-		boolean bucketExists = s3.listBuckets().stream()
-				.anyMatch(b -> b.getName().equals(BUCKET_NAME));
-		if (!bucketExists) {
-			createBucket(s3);
-		}
-	}
+        return AmazonS3EncryptionClientV2Builder.standard()
+                .enablePathStyleAccess()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(s3Endpoint, Regions.US_EAST_1.getName()))
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withCryptoConfiguration(new CryptoConfigurationV2().withCryptoMode(CryptoMode.AuthenticatedEncryption))
+                .withEncryptionMaterialsProvider(new StaticEncryptionMaterialsProvider(new EncryptionMaterials(secretKey)))
+                .build();
+    }
 
-	private void configureBucketLifecycle(AmazonS3 s3) {
-		List<BucketLifecycleConfiguration.Rule> ruleList = new ArrayList<>();
-		ruleList.add(new BucketLifecycleConfiguration.Rule()
-				.withId("Object lifecycle rule")
-				.withFilter(new LifecycleFilter())
-				.withStatus(BucketLifecycleConfiguration.ENABLED)
-				.withExpirationInDays(60));
+    private void ensureBucketExists(AmazonS3 s3) {
+        boolean bucketExists = s3.listBuckets().stream()
+                .anyMatch(b -> b.getName().equals(BUCKET_NAME));
+        if (!bucketExists) {
+            createBucket(s3);
+        }
+    }
 
-		BucketLifecycleConfiguration configuration = new BucketLifecycleConfiguration();
-		configuration.setRules(ruleList);
+    private void configureBucketLifecycle(AmazonS3 s3) {
+        List<BucketLifecycleConfiguration.Rule> ruleList = new ArrayList<>();
+        ruleList.add(new BucketLifecycleConfiguration.Rule()
+                .withId("Object lifecycle rule")
+                .withFilter(new LifecycleFilter())
+                .withStatus(BucketLifecycleConfiguration.ENABLED)
+                .withExpirationInDays(60));
 
-		s3.setBucketLifecycleConfiguration(BUCKET_NAME, configuration);
-	}
+        BucketLifecycleConfiguration configuration = new BucketLifecycleConfiguration();
+        configuration.setRules(ruleList);
 
-	private void configureBucketAccessPolicy(AmazonS3 s3) {
-		Statement allowDokarkivRead = new Statement(Statement.Effect.Allow)
-				.withPrincipals(new Principal("arn:aws:iam:::user/dokdistsentralprint"), new Principal("arn:aws:iam:::user/dokdisteformidling"))
-				.withActions(S3Actions.GetObject)
-				.withResources(new S3ObjectResource(BUCKET_NAME, "*"));
-		Policy accessPolicy = new Policy().withStatements(allowDokarkivRead);
-		s3.setBucketPolicy(BUCKET_NAME, accessPolicy.toJson());
-	}
+        s3.setBucketLifecycleConfiguration(BUCKET_NAME, configuration);
+    }
 
-	private void createBucket(AmazonS3 s3) {
-		s3.createBucket(new CreateBucketRequest(BUCKET_NAME)
-				.withCannedAcl(CannedAccessControlList.Private));
-	}
+    private void configureBucketAccessPolicy(AmazonS3 s3) {
+        Statement allowDokarkivRead = new Statement(Statement.Effect.Allow)
+                .withPrincipals(new Principal("arn:aws:iam:::user/dokdistsentralprint"), new Principal("arn:aws:iam:::user/dokdisteformidling"))
+                .withActions(S3Actions.GetObject)
+                .withResources(new S3ObjectResource(BUCKET_NAME, "*"));
+        Policy accessPolicy = new Policy().withStatements(allowDokarkivRead);
+        s3.setBucketPolicy(BUCKET_NAME, accessPolicy.toJson());
+    }
+
+    private void createBucket(AmazonS3 s3) {
+        s3.createBucket(new CreateBucketRequest(BUCKET_NAME)
+                .withCannedAcl(CannedAccessControlList.Private));
+    }
 
 
-	private SecretKey key(String passphrase) {
-		if (passphrase.getBytes().length == 32) {
-			return new SecretKeySpec(passphrase.getBytes(), "AES");
-		} else {
-			throw new ValidationException("Passordet for s3Storage sin AES må være 256 bit");
-		}
-	}
+    private SecretKey key(String passphrase) {
+        if (passphrase.getBytes().length == 32) {
+            return new SecretKeySpec(passphrase.getBytes(), "AES");
+        } else {
+            throw new ValidationException("Passordet for s3Storage sin AES må være 256 bit");
+        }
+    }
 }
