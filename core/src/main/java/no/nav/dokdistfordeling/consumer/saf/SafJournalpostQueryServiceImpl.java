a823f61defee40e28d9fdf9e76450c3b758026a8
A157857 <tim.aasen@nav.no>
2021-04-28 13:31:46 +0200

    Legge til tittel query for testing

--- core/src/main/java/no/nav/dokdistfordeling/consumer/saf/SafJournalpostQueryServiceImpl.java
+++ core/src/main/java/no/nav/dokdistfordeling/consumer/saf/SafJournalpostQueryServiceImpl.java
@@ -1,16 +1,19 @@
 package no.nav.dokdistfordeling.consumer.saf;
 
 
+import lombok.extern.slf4j.Slf4j;
 import no.nav.dokdistfordeling.consumer.saf.graphql.GraphQLRequest;
 import no.nav.dokdistfordeling.consumer.saf.graphql.JournalpostToMapper;
 import no.nav.dokdistfordeling.consumer.saf.graphql.JournalpostToValidator;
 import no.nav.dokdistfordeling.consumer.saf.graphql.SafGraphqlConsumer;
 import no.nav.dokdistfordeling.consumer.saf.journalpost.Journalpost;
+import no.nav.dokdistfordeling.consumer.saf.journalpost.SafJournalpostTo;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 
 @Component
+@Slf4j
 public class SafJournalpostQueryServiceImpl implements SafJournalpostQueryService {
 
 	private static final String JOURNALPOST_QUERY =
@@ -51,13 +54,25 @@ public class SafJournalpostQueryServiceImpl implements SafJournalpostQueryServic
 
 	public Journalpost hentJournalpost(String journalpostid, String authorizationHeader) {
 
-		return journalpostMapper.map(
-				journalpostToValidator.validateAndReturn(
-						safGraphqlConsumer.performQuery(GraphQLRequest.builder()
-								.query(JOURNALPOST_QUERY)
-								.operationName("journalpost")
-								.variables(Collections.singletonMap("queryJournalpostId", journalpostid))
-								.build(), authorizationHeader))
-		);
+
+		SafJournalpostTo safJournalpostTo = safGraphqlConsumer.performQuery(GraphQLRequest.builder()
+				.query(JOURNALPOST_QUERY)
+				.operationName("journalpost")
+				.variables(Collections.singletonMap("queryJournalpostId", journalpostid))
+				.build(), authorizationHeader);
+
+		log.info("hentJournalpost - tittel {}", safJournalpostTo.getTittel());
+
+		return journalpostMapper.map(journalpostToValidator.validateAndReturn(safJournalpostTo));
+//
+//
+//		return journalpostMapper.map(
+//				journalpostToValidator.validateAndReturn(
+//						safGraphqlConsumer.performQuery(GraphQLRequest.builder()
+//								.query(JOURNALPOST_QUERY)
+//								.operationName("journalpost")
+//								.variables(Collections.singletonMap("queryJournalpostId", journalpostid))
+//								.build(), authorizationHeader))
+//		);
 	}
 }
