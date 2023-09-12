5ce1e8e09d5871459cd1cf2b39c68f20616d5dc0
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2023-09-12 15:42:13 +0200

    MMA-7063 Fikset workflows. Oppdatert swagger docs med 410 status
    * Oppdatert camel

--- rdist002/src/main/java/no/nav/dokdistfordeling/springdoc/SwaggerRestDistribuerJournalpost.java
+++ rdist002/src/main/java/no/nav/dokdistfordeling/springdoc/SwaggerRestDistribuerJournalpost.java
@@ -22,7 +22,8 @@ import java.lang.annotation.Target;
 		@ApiResponse(responseCode = "401", description = "* Bruker mangler tilgang for å vise journalposten.\n* Ugyldig OIDC token.", content = @Content),
 		@ApiResponse(responseCode = "404", description = "Journalposten ble ikke funnet.", content = @Content),
 		@ApiResponse(responseCode = "409", description = "Journalposten er allerede distribuert."),
-		@ApiResponse(responseCode = "500", description = " Teknisk feil under prosessering av forsendelse.", content = @Content)
+		@ApiResponse(responseCode = "410", description = "Journalpost kan ikke distribueres. Bruker er død og har ukjent adresse."),
+		@ApiResponse(responseCode = "500", description = "Teknisk feil under prosessering av forsendelse.", content = @Content)
 }
 )
 public @interface SwaggerRestDistribuerJournalpost {
