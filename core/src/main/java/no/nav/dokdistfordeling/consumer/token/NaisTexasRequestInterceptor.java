64667d7080c3561b612a8ffa636e0f4a3e5c9689
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2025-07-21 15:31:36 +0200

    MMA-8212 La til konfigurerbar readTimeout for RestClient bean
    * Bruker built-in JDK `HttpClient`
    * Fixet setting av `Nav-Callid` headeren

--- core/src/main/java/no/nav/dokdistfordeling/consumer/token/NaisTexasRequestInterceptor.java
+++ core/src/main/java/no/nav/dokdistfordeling/consumer/token/NaisTexasRequestInterceptor.java
@@ -1,5 +1,6 @@
 package no.nav.dokdistfordeling.consumer.token;
 
+import org.slf4j.MDC;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
@@ -7,6 +8,11 @@ import org.springframework.http.client.ClientHttpResponse;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
+
+import static no.nav.dokdistfordeling.constants.Constants.CALL_ID;
+import static no.nav.dokdistfordeling.consumer.NavHeaders.NAV_CALL_ID;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 public class NaisTexasRequestInterceptor implements ClientHttpRequestInterceptor {
 
@@ -20,11 +26,17 @@ public class NaisTexasRequestInterceptor implements ClientHttpRequestInterceptor
 	@Override
 	public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
 		Map<String, Object> attributes = request.getAttributes();
-		if(!attributes.containsKey(TARGET_SCOPE)) {
+		if (!attributes.containsKey(TARGET_SCOPE)) {
 			throw new IllegalArgumentException("Kan ikke bruke denne restClient uten at targetScope attributtet er satt");
 		}
 		String targetScope = (String) attributes.get(TARGET_SCOPE);
 		request.getHeaders().setBearerAuth(naisTexasConsumer.getSystemToken(targetScope));
+		request.getHeaders().add(NAV_CALL_ID, getMDCCallId());
 		return execution.execute(request, body);
+	}
+
+	private static String getMDCCallId() {
+		String callId = MDC.get(CALL_ID);
+		return isNotBlank(callId) ? callId : UUID.randomUUID().toString();
 	}
 }
