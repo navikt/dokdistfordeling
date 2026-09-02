41073e58f47aac0dd509c947fc6603448a88ea7e
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2026-09-02 14:27:45 +0200

    MMA-8865 La tilbake validering av mottaker idtype
    * La til test

--- rdist002/src/test/java/no/nav/dokdistfordeling/itest/Rdist002IT.java
+++ rdist002/src/test/java/no/nav/dokdistfordeling/itest/Rdist002IT.java
@@ -835,6 +835,22 @@ class Rdist002IT extends AbstractOauth2Test {
 	}
 
 	@Test
+	void shouldReturnBadRequestIfJournalpostMottakerHprnr() {
+		stubSafGraphQl("saf/safgraphql-hprnr.json");
+		stubBestemDistribusjonskanal("bestemdistribusjonskanal/print.json");
+		setupDatabase();
+
+		HttpEntity<DistribuerJournalpostRequestTo> requestEntity = new HttpEntity<>(
+				createDistribuerJournalpostToBuilder()
+						.adresse(null)
+						.build(),
+				createHappyPathHeaders());
+
+		String body = callDistribuerJournalpostAndAssertErrorResponseCode(requestEntity, BAD_REQUEST);
+		assertThat(body).contains("Journalpost.avsenderMottaker.idType må være FNR eller ORGNR hvis postadresse ikke oppgis i request.");
+	}
+
+	@Test
 	void shouldReturnUnauthorizedWhenInvalidToken() {
 		HttpEntity<DistribuerJournalpostRequestTo> requestEntity = new HttpEntity<>(
 				createDistribuerJournalpostToBuilder().build(),
